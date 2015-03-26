12fd3dd5e39d1bfbf6682eec80b2013c49202b40
Muhammad Safraz <mr.safraz@gmail.com>
2015-03-26 14:45:48 +0530

    Add legacy widget support

--- _framework/ui/widget.js
+++ _framework/ui/widget.js
@@ -1,6 +1,9 @@
 import {annotate, TransientScope} from 'di';
 
 export class Widget {
+	static support(widgetFn){
+		annotate(widgetFn, new TransientScope);
+	}
 }
 
 annotate(Widget, new TransientScope);
