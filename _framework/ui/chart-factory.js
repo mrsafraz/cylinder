eccb29686941065d2eaabede8f359cbb0fc3b9b8
Muhammad Safraz <mr.safraz@gmail.com>
2015-04-07 19:26:47 +0530

    Support color parameter from settings

--- _framework/ui/chart-factory.js
+++ _framework/ui/chart-factory.js
@@ -65,61 +65,20 @@ export class ChartFactory {
 		for(var key in options){
 			chart[key] = options[key];
 		}
-		// if(chart.data && !chart.data.color){
-		// 	chart.data.color = (color, d)=> {
-		// 		var key = d.id || d;
-		// 		return this.colorManager.getColor(key);
-		// 	};
-		// }
-		chart.color = {};
-		var cat1 = d3.scale.category20c().range().concat([]),
-		cat2 = d3.scale.category20b().range().concat([]),
-		cat3 = d3.scale.category20().range().concat([]);
+		if(!chart.color){
+			chart.color = {};
+			var cat1 = d3.scale.category20c().range().concat([]),
+			cat2 = d3.scale.category20b().range().concat([]);
+			chart.color.pattern = [].concat(
+				[
+				cat1[0], cat1[1], //cat1[2],
+				cat1[12], cat1[13], //cat1[14],
+				cat1[16], cat1[17], //cat1[18],
 
-		chart.color.pattern = [].concat(
-			// cat1.splice(0, 3),
-			// cat1.splice(3, 3),
-			cat1,
-			[
-				'rgb(107, 174, 214)',
-				'rgb(158, 202, 225)',
-
-				// 'rgb(198, 219, 239)',
-
-				// 'rgb(165, 81, 148)',
-				'rgb(206, 109, 189)',
-				'rgb(222, 158, 214)',
-
-				'rgb(231, 150, 156)',
-
-				'rgb(107, 110, 207)',
-				'rgb(156, 158, 222)',
-				//
-				'rgb(150, 150, 150)',
-				'rgb(189, 189, 189)',
-			],
-			// cat1.splice(0, 5),
-			// cat1.splice(4, 5)
-			// cat2.reverse()
-			// cat1
-			[]
-		);
-
-		chart.color.pattern = [].concat(
-			// cat1,
-			// cat3,
-			// cat3.splice(0, 3),
-
-			//
-			[
-			cat1[0], cat1[1], //cat1[2],
-			cat1[12], cat1[13], //cat1[14],
-			cat1[16], cat1[17], //cat1[18],
-
-			cat2[0], cat2[1], //cat2[2],
-			],
+				cat2[0], cat2[1], //cat2[2],
+				],
 			[]);
-		// chart.color.pattern = cat3;
+		}
 		return chart;
 	}
 }
