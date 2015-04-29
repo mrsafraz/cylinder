4e5c05203968185e6b20212696fc55aadf1481ce
Muhammad Safraz <mr.safraz@gmail.com>
2015-04-29 21:17:59 +0530

    keep the authenticatio provider minimal

--- _framework/auth/authentication-provider.js
+++ _framework/auth/authentication-provider.js
@@ -1,104 +1,34 @@
-import $ from 'jquery';
-import {Config} from '../core/config';
-import {Encoder} from './encoder';
-import {WsseRequest} from './wsse-request';
-
-var _currentUser = {};
+var _users = {};
 
 export class AuthenticationProvider {
 
-  constructor(encoder: Encoder, request: WsseRequest, config: Config){
-    this.encoder = encoder;
-    this.request = request;
-    this.config = config;
-    this.authUrl = this.config.authUrl;
-  }
-
   get currentUser(){
-    return {
-      id: _currentUser.id,
-      username: _currentUser.username,
-      salt: _currentUser.salt,
-      roles: _currentUser.roles,
-    }
+    return {};
   }
 
-  doValidateLogin(){
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: this.authUrl + '/validate',
-        dataType: 'json'
-      }).done((data)=> {
-        if(data.roles && data.roles.length){
-          resolve(data);
-        }
-        else {
-          reject(data.error || new Error('Invalid user'));
-        }
-      }).fail((error)=> {
-        reject(new Error('Login failed.'));
-      });
-    });
+  addUser(username, password){
+    _users[username] = password;
+  }
+
+  static addUser(username, password){
+    _users[username] = password;
   }
 
   doLogout(){
     return new Promise((resolve, reject) => {
-      $.ajax({
-        url: this.authUrl + '/logout',
-        dataType: 'json'
-      }).done((data)=> {
-        alert("? " + data);
-        if(data.success){
-          resolve();
-        }
-        else {
-          reject(data.error || new Error('Logout failed. Error Occurred.'));
-        }
-      }).fail((error)=> {
-        reject(new Error('Logout failed. Unexpected Error.'));
-      });
+      resolve();
     });
   }
 
   doLogin(username, plainPassword){
     return new Promise((resolve, reject) => {
-      $.ajax({
-        url: this.authUrl + '/login',
-        data: {
-            username: username
-        },
-        beforeSend: function(jqXHR) {
-            jqXHR.headers = {};
-        },
-        dataType: 'json',
-        type: 'POST'
-      }).done((data)=> {
-        if(data.error){
-          reject(new Error(data.error));
-        }
-        else if(data.salt) {
-          var encodedPassword = this.encoder.encryptPassword
-            (plainPassword, data.salt);
-          $.ajaxSetup({
-            beforeSend: (jqXHR, settings) => {
-              var wsse = this.request.create(username, encodedPassword);
-              // jqXHR.setRequestHeader('Authorization', 'WSSE profile="UsernameToken"');
-              jqXHR.setRequestHeader('x-wsse', wsse.token);
-            }
-          });
-          this.doValidateLogin().then((userData) => {
-            _currentUser.roles = userData.roles;
-            _currentUser.id = userData.id;
-            _currentUser.username = username;
-            _currentUser.salt = data.salt;
-            resolve(userData);
-          }, (error)=> {
-            reject(error);
-          });
-        }
-      }).fail((error)=> {
-        reject(new Error('Unexpected Network Error'));
-      });
+      var password = _users[username];
+      if(password && password == plainPassword){
+        resolve();
+      }
+      else {
+        reject(new Error('Invalid username or password'));
+      }
     });
   }
 
@@ -126,29 +56,7 @@ export class AuthenticationProvider {
 
   updatePassword(username, newPassword, existingPassword){
     return new Promise((resolve, reject) => {
-      var newCredentials = this.encoder.createCredentials(newPassword);
-      var existingSalt = this.currentUser.salt;
-      var existingPasswordEncrypted = this.encoder.encryptPassword(existingPassword, existingSalt);
-      var data = {
-        credentials: newCredentials,
-        hash: this.encoder.encryptPassword(newCredentials, existingPasswordEncrypted)
-      }
-      $.ajax({
-        url: this.authUrl + '/account',
-        data: data,
-        method: 'POST',
-        dataType: 'json',
-      }).done((data)=> {
-        if(data.error){
-          reject(new Error(data.error));
-        }
-        else {
-          this.doLogin(username, newPassword);
-          resolve();
-        }
-      }).fail((error)=> {
-        reject(error);
-      });
+      resolve();
     });
   }
 
